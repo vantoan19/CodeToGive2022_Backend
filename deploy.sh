7e7257158ebac604e1b8ed974f7869a3066fd233
Toan Tran <thetopoftheworld19@gmail.com>
2022-09-11 01:05:56 +0200

    fix missing openapi

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 echo "Building api image..."
-echo "docker build -t vantoan11/codetogive2022-api:$SHA -f ./server/Dockerfile ./server"
 docker build -t vantoan11/codetogive2022-api:$SHA -f ./server/Dockerfile ./server
+docker build -t vantoan11/codetogive2022-api:latest -f ./server/Dockerfile ./server
 echo "Pushing api image into docker hub..."
 docker push vantoan11/codetogive2022-api:$SHA
+docker push vantoan11/codetogive2022-api:latest
 kubectl apply -f kubernetes
 kubectl set image deployments/api-deployment api=vantoan11/codetogive2022-api:$SHA
