b299e465115adea441a16564244a509635d9f0b4
Toan Tran <thetopoftheworld19@gmail.com>
2022-09-10 23:20:23 +0200

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Building api image..."
-docker build -t vantoan11/codetogive2022_api:latest -t vantoan11/codetogive2022_api:$SHA ./server
+docker build -t vantoan11/codetogive2022-api:$SHA -f ./server/Dockerfile ./server
+docker tag
 echo "Pushing api image into docker hub..."
-docker push vantoan11/codetogive2022_api:$SHA
-docker push vantoan11/codetogive2022_api:latest
+docker push vantoan11/codetogive2022-api:$SHA
 kubectl apply -f kubernetes
-kubectl set image deployment/api-deployment api=vantoan11/codetogive2022_api:$SHA
+kubectl set image deployment/api-deployment api=vantoan11/codetogive2022-api:$SHA
