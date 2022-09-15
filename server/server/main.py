f30c81048865340a858a0100246a9401d1cab173
Toan Tran <thetopoftheworld19@gmail.com>
2022-09-16 00:51:47 +0200

    fix client nginx

--- server/server/main.py
+++ server/server/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
-app = FastAPI(title="CodeToGive backend server")
+app = FastAPI(docs_url="/api/v1/docs", openapi_url="/api/v1/openapi.json")
 
-@app.get("/")
+@app.get("/api/v1/hello")
 async def root():
     return {"message": "Hello World!"}
     
