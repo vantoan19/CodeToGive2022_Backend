ad8d8b265d2cf336496d2d5befdf49f0e52f4fda
amigo1121 <hoainam.hk14.cqt@gmail.com>
2022-09-29 22:14:20 +0200

    change origins

--- server/server/main.py
+++ server/server/main.py
@@ -4,51 +4,15 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(docs_url="/api/v1/docs", openapi_url="/api/v1/openapi.json")
 
+origins = ["*"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex="http://34.116.151.108/client.*"
+    allow_origin=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-mock_jobs = [{
-    "id": 1,
-    "label": "Librarian",
-    "match_value": 72,
-    "details": {
-      "company_name": "Canary inc.",
-      "about": "This company is legit, no cap, 4real",
-      "description": "U will need to do this, and that, this skill, that skill"
-    },
-    "image": "https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGlicmFyeXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-  },
-  {
-    "id": 2,
-    "label": "Recptionist",
-    "match_value": 60,
-    "details": {
-        "company_name": "Dunder Mifflin Inc.",
-        "about": "Paper company",
-        "description": "Answer the phone, make some copies,etc.",
-    },
-    "image": "https://images.unsplash.com/photo-1556741533-6e6a62bd8b49?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmVjZXB0aW9uaXN0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-  },
-  {
-    "id": 3,
-    "label": "Bartender",
-    "match_value": 90,
-    "details": {
-        "company_name": "Black Box",
-        "about": "Club",
-        "description": "Make and serves drinks"
-    },
-    "image": "https://images.unsplash.com/photo-1595751866979-de6e9d606220?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YmFydGVuZGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-  }
-  ]
-
-
 
 @app.get("/api/v1/hello")
 async def root():
