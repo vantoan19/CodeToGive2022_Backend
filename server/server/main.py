b511d66cab26a381471e4264c7514145bf5829d4
Toby <52330522+nvminhhieu@users.noreply.github.com>
2022-09-30 16:38:24 +0200

    add work-motivation with diff func name

--- server/server/main.py
+++ server/server/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-# from mock_data import mock_jobs,mock_work_motivation_test 
+from mock_data import mock_jobs, mock_work_motivation_test 
 
 app = FastAPI(docs_url="/api/v1/docs", openapi_url="/api/v1/openapi.json")
 
@@ -16,42 +16,6 @@ app.add_middleware(
 )
 
 
-mock_jobs = [{
-    "id": 1,
-    "label": "Librarian",
-    "match_value": 72,
-    "details": {
-      "company_name": "Canary inc.",
-      "about": "This company is legit, no cap, 4real",
-      "description": "U will need to do this, and that, this skill, that skill"
-    },
-    "image": "https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGlicmFyeXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-  },
-  {
-    "id": 2,
-    "label": "Recptionist",
-    "match_value": 60,
-    "details": {
-        "company_name": "Dunder Mifflin Inc.",
-        "about": "Paper company",
-        "description": "Answer the phone, make some copies,etc.",
-    },
-    "image": "https://images.unsplash.com/photo-1556741533-6e6a62bd8b49?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmVjZXB0aW9uaXN0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-  },
-  {
-    "id": 3,
-    "label": "Bartender",
-    "match_value": 90,
-    "details": {
-        "company_name": "Black Box",
-        "about": "Club",
-        "description": "Make and serves drinks"
-    },
-    "image": "https://images.unsplash.com/photo-1595751866979-de6e9d606220?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YmFydGVuZGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-  }
-  ]
-
-
 
 @app.get("/api/v1/hello")
 async def root():
@@ -62,4 +26,7 @@ async def root():
 async def get_jobs():
     return mock_jobs
 
+@app.get("/api/v1/user/work-motivation-test/assessment" )
+async def get_work_motivation_test():
+    return mock_work_motivation_test
 
