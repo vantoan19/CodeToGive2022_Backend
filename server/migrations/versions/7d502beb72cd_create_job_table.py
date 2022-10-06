e8b3c991b4fd2351ff0ef0159f4d1190dc44397d
Toan Tran <thetopoftheworld19@gmail.com>
2022-10-06 02:05:40 +0200

    migration changes

--- server/migrations/versions/7d502beb72cd_create_job_table.py
+++ server/migrations/versions/7d502beb72cd_create_job_table.py
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = '7d502beb72cd'
-down_revision = 'd2478ac979a4'
+revision = "7d502beb72cd"
+down_revision = "d2478ac979a4"
 branch_labels = None
 depends_on = None
 
@@ -28,18 +28,56 @@ def upgrade() -> None:
     )
     op.create_table(
         "job2label",
-        sa.Column("job_id", sa.Integer(), sa.ForeignKey("jobs.id"), index=True, nullable=False),
-        sa.Column("label_id", sa.Integer(), sa.ForeignKey("labels.id"), index=True, nullable=False),
+        sa.Column(
+            "job_id", sa.Integer(), sa.ForeignKey("jobs.id"), index=True, nullable=False
+        ),
+        sa.Column(
+            "label_id",
+            sa.Integer(),
+            sa.ForeignKey("labels.id"),
+            index=True,
+            nullable=False,
+        ),
         sa.Column("lower_importance_bound", sa.Integer(), default=1),
-        sa.Column("upper_importance_bound", sa.Integer(), default=5)
+        sa.Column("upper_importance_bound", sa.Integer(), default=5),
     )
-    op.add_column("addresses", sa.Column("job_id", sa.Integer(), sa.ForeignKey("jobs.id"), nullable=True))
-    op.create_foreign_key("address_job_fk", source_table="addresses", referent_table="jobs",
-                          local_cols=["job_id"], remote_cols=["id"], ondelete="CASCADE", onupdate="CASCADE")
-    op.create_foreign_key("job2label_job_fk", source_table="job2label", referent_table="jobs",
-                          local_cols=["job_id"], remote_cols=["id"], ondelete="CASCADE", onupdate="CASCADE")
-    op.create_foreign_key("job2label_label_fk", source_table="job2label", referent_table="labels",
-                          local_cols=["label_id"], remote_cols=["id"], ondelete="CASCADE", onupdate="CASCADE")
+    op.add_column(
+        "addresses",
+        sa.Column("job_id", sa.Integer(), sa.ForeignKey("jobs.id"), nullable=True),
+    )
+    op.create_foreign_key(
+        "address_job_fk",
+        source_table="addresses",
+        referent_table="jobs",
+        local_cols=["job_id"],
+        remote_cols=["id"],
+        ondelete="CASCADE",
+        onupdate="CASCADE",
+    )
+    op.create_foreign_key(
+        "job2label_job_fk",
+        source_table="job2label",
+        referent_table="jobs",
+        local_cols=["job_id"],
+        remote_cols=["id"],
+        ondelete="CASCADE",
+        onupdate="CASCADE",
+    )
+    op.create_foreign_key(
+        "job2label_label_fk",
+        source_table="job2label",
+        referent_table="labels",
+        local_cols=["label_id"],
+        remote_cols=["id"],
+        ondelete="CASCADE",
+        onupdate="CASCADE",
+    )
+
 
 def downgrade() -> None:
-    op.drop_constraint()
+    op.drop_constraint("job2label_label_fk", table_name="labels")
+    op.drop_constraint("job2label_job_fk", table_name="jobs")
+    op.drop_constraint("address_job_fk", table_name="jobs")
+    op.drop_column("addresses", "job_id")
+    op.drop_table("job2label")
+    op.drop_table("jobs")
